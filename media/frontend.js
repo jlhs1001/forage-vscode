52b8d9332432ef7c677f51fed5843c2c1d131fcb
Liam Seewald <liam@root3.dev>
2022-02-24 15:12:22 -0500

    fix match render bug

--- media/frontend.js
+++ media/frontend.js
@@ -38,30 +38,36 @@ const querySource = (query, source) => {
     const linesLength = lines.length;
     let formattedLine = "";
     let prevMatchIndex = 0;
-    let matchEnd = 0;;
+    let matchEnd = 0;
 
     // loop over ALL lines
     for (let i = 0; i < linesLength; i++) {
         let matches = [...lines[i].matchAll(re)];
-        for (let n = 0; n < matches.length; n++) {
-            let matchIndex = matches[n].index;
-            matchEnd = matchIndex + matches[n][0].length;
-            console.log(matchIndex);
-            console.log(lines[i].substring(prevMatchIndex - 3, matchIndex));
-            formattedLine +=
-                lines[i].substring(prevMatchIndex, matchIndex) +
-                '<span class="highlight" style="background-color: red; color: blue;">' +
-                lines[i].substring(matchIndex, matchEnd) +
-                '</span>';
-            prevMatchIndex = matchIndex + matches[n][0].length;
+        if (matches.length !== 0) {
+            for (let n = 0; n < matches.length; n++) {
+                let matchIndex = matches[n].index;
+                matchEnd = matchIndex + matches[n][0].length;
+                console.log(matchIndex);
+                console.log(lines[i].substring(prevMatchIndex - 3, matchIndex));
+                formattedLine +=
+                    escapeHTML(lines[i].substring(prevMatchIndex, matchIndex)) +
+                    '<span class="highlight" style="background-color: red; color: blue;">' +
+                    escapeHTML(lines[i].substring(matchIndex, matchEnd)) +
+                    '</span>';
+                prevMatchIndex = matchIndex + matches[n][0].length;
+            }
+            result.push(formattedLine + lines[i].substring(matchEnd, lines[i].length));
+            formattedLine = "";
+            prevMatchIndex = 0;
+            matchEnd = 0;
+        } else {
+            result.push(escapeHTML(lines[i]));
         }
-        result.push(formattedLine + lines[i].substring(matchEnd, lines[i].length));
-        formattedLine = "";
-        prevMatchIndex = 0;
     }
 
     return result;
 };
+
 
 const res = querySource("[0-9]+", "34\n2345fgg24\n542\ng245\ng42\n5n2yhn35n63\n45\n542\n55432432\n");
 console.log(res);
