61403dd9209a188911747050c4b606da85d9361e
Liam Seewald <liam@root3.dev>
2022-02-24 14:42:00 -0500

    fix querySource match bug in fronted

--- media/frontend.js
+++ media/frontend.js
@@ -24,7 +24,7 @@ const escapeHTML = str => str.replace(/[&<>'"]/g,
 
 const querySource = (query, source) => {
     if (query.length === 0) {
-        resultPanel.innerHTML = escapeHtml(source);
+        resultPanel.innerHTML = escapeHTML(source);
         return false;
     }
 
@@ -37,21 +37,25 @@ const querySource = (query, source) => {
     // cached lengths (for... negligible speedups...)
     const linesLength = lines.length;
     let formattedLine = "";
-
     let prevMatchIndex = 0;
+    let matchEnd = 0;;
 
     // loop over ALL lines
     for (let i = 0; i < linesLength; i++) {
         let matches = [...lines[i].matchAll(re)];
         for (let n = 0; n < matches.length; n++) {
             let matchIndex = matches[n].index;
+            matchEnd = matchIndex + matches[n][0].length;
+            console.log(matchIndex);
+            console.log(lines[i].substring(prevMatchIndex - 3, matchIndex));
             formattedLine +=
-                escapeHTML(lines[i].substring(prevMatchIndex, matchIndex)) +
+                lines[i].substring(prevMatchIndex, matchIndex) +
                 '<span class="highlight" style="background-color: red; color: blue;">' +
-                escapeHTML(lines[i].substring(matchIndex, prevMatchIndex += matchIndex + matches[n][0].length)) +
+                lines[i].substring(matchIndex, matchEnd) +
                 '</span>';
+            prevMatchIndex = matchIndex + matches[n][0].length;
         }
-        result.push(formattedLine);
+        result.push(formattedLine + lines[i].substring(matchEnd, lines[i].length));
         formattedLine = "";
         prevMatchIndex = 0;
     }
